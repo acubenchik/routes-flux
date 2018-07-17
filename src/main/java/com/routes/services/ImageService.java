c7cde80403aacec8d20624352ac94f973f23b8e4
acubenchik <alex.usavchenko@gmail.com>
2018-07-17 16:13:36 +0200

    []: fixed image reading;

--- src/main/java/com/routes/services/ImageService.java
+++ src/main/java/com/routes/services/ImageService.java
@@ -1,13 +1,10 @@
 package com.routes.services;
 
 import org.apache.commons.io.IOUtils;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
 import java.util.Base64;
 
 @Component
@@ -15,7 +12,7 @@ public class ImageService implements IImageService {
 
     @Override
     public String getImage(String id) throws IOException {
-        InputStream file = getClass().getResourceAsStream("/" + id);
+        InputStream file = getClass().getResourceAsStream("/" + id + ".jpeg");
         return Base64.getEncoder().withoutPadding().encodeToString(IOUtils.toByteArray(file));
     }
 
